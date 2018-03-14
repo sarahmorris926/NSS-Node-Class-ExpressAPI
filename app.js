e752bf5b9a9fbca299cd7da86b24c828e9bb4e37
Sarah Morris <sarah.morris9@gmail.com>
2018-03-14 10:53:48 -0500

    added build-db to start a database in sqlite

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+require('dotenv').config(); // this will take any variables from .env file and add them as variables you can use in app.js
 const app = express();
 const port = process.env.PORT || 8080;
 
